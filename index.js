8459bd645cfe98af34de1fe1bb5abe96c2cf8ecc
Nicholas Lindley <me@thisoneplace.com>
2014-11-04 08:30:00 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+function validate(vin) {
+  vin = vin.toLowerCase();
+  if (!/^[abcdefghjklmnprstuvwxyz0123456789]{17}$/.test(vin)) {
+    return false;
+  }
+
+  var transliterationTable = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'j': 1,
+    'k': 2,
+    'l': 3,
+    'm': 4,
+    'n': 5,
+    'p': 7,
+    'r': 9,
+    's': 2,
+    't': 3,
+    'u': 4,
+    'v': 5,
+    'w': 6,
+    'x': 7,
+    'y': 8,
+    'z': 9
+  };
+
+  var weightsTable = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2];
+  var sum = 0;
+
+  for (var i = 0; i < vin.length; ++i) {
+    sum += transliterationTable[vin[i]] * weightsTable[i];
+  }
+
+  var mod = sum % 11;
+  return mod === 10 ? vin[8] === 'x' : vin[8] == mod;
+}
+
+module.exports = {
+  validate: validate
+};
---
+++ test/vin.js
@@ -0,0 +1,145 @@
+var expect = require('chai').expect;
+var VIN = require('../');
+
+describe('Given a 16 digit VIN', function() {
+  var vin = 'WVWUK63B92P54681';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 18 digit VIN', function() {
+  var vin = 'WVWUK63B92P5468188';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an i', function() {
+  var vin = 'WVWUK63B92P54681i';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an I', function() {
+  var vin = 'WVWUK63B92P54681I';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an o', function() {
+  var vin = 'WVWUK63B92P54681o';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an O', function() {
+  var vin = 'WVWUK63B92P54681O';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an q', function() {
+  var vin = 'WVWUK63B92P54681q';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a 17 digit VIN with an Q', function() {
+  var vin = 'WVWUK63B92P54681Q';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a valid VIN with lowercase letters', function() {
+  var vin = 'wvwuk63b92p546818';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should pass', function() {
+      expect(valid).to.be.true;
+    });
+  });
+});
+
+describe('Given a VIN with an incorrect check digit', function() {
+  var vin = 'WVWUK63B82P546818';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should fail', function() {
+      expect(valid).to.be.false;
+    });
+  });
+});
+
+describe('Given a VIN with a correct check digit', function() {
+  var vin = '11111111111111111';
+
+  describe('When the VIN is validated', function() {
+    var valid;
+    beforeEach(function() { valid = VIN.validate(vin); })
+
+    it('Then it should pass', function() {
+      expect(valid).to.be.true;
+    });
+  });
+});
